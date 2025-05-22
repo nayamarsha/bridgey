f343e81f5f06709869f504f8fe09b2e7e47e1cdb
nayamarsha <nayamarsha04@gmail.com>
2025-05-22 12:05:40 +0700

    Add splashscreen

--- app/src/main/java/com/example/bridgey2/MainActivity.kt
+++ app/src/main/java/com/example/bridgey2/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bridgey2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 //import com.example.bridgey2.Fragments.ScheduleFragment
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
