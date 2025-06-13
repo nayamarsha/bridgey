0a14db54767587e5e2b7a58b30eb1834703aa2cb
nayamarsha <nayamarsha04@gmail.com>
2025-06-13 09:45:58 +0700

    Fix some error

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,4 @@ dependencies {
     implementation ("androidx.viewpager2:viewpager2:1.0.0")
     implementation ("com.google.android.material:material:1.12.0")
 
-}
+}}
--- app/src/main/java/com/example/bridgey2/EditProfileActivity.kt
+++ app/src/main/java/com/example/bridgey2/EditProfileActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.bridgey2.R
 
 class EditProfileActivity : AppCompatActivity() {
 
