52a299d2a474d9d2e95ad43674c49c36efefd8d5
hubnigitle <maulenirh@gmail.com>
2025-06-19 13:51:18 +0700

    post proposal done

--- app/src/main/java/com/example/bridgey2/PostProposalActivity.kt
+++ app/src/main/java/com/example/bridgey2/PostProposalActivity.kt
@@ -2,15 +2,19 @@ package com.example.bridgey2
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
+import androidx.navigation.findNavController
 
 class PostProposalActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Set layout
+        enableEdgeToEdge()
         setContentView(R.layout.activity_post_proposal)
 
         // Ambil root view dengan ID rootLayout
@@ -21,6 +25,14 @@ class PostProposalActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<ImageButton>(R.id.iconCancel).setOnClickListener {
+            finish()
+        }
+
+        findViewById<MaterialButton>(R.id.postButton).setOnClickListener {
+            finish()
         }
     }
 }
